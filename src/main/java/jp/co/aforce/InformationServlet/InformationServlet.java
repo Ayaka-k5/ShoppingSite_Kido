d642899c94fecfc20fa81cbd88a83132d3b17f73
Ayaka-k5 <a.kido@ks-japan.jp>
2024-06-14 10:37:25 +0900

    6/14 10:37

--- src/main/java/jp/co/aforce/InformationServlet/InformationServlet.java
+++ src/main/java/jp/co/aforce/InformationServlet/InformationServlet.java
@@ -28,18 +28,37 @@ public class InformationServlet extends HttpServlet {
 //			String customer_phonenumber = request.getParameter("customer_telephone");
 //			String customer_address = request.getParameter("customer_address");
 
-			InformationDAO dao = new InformationDAO();
-			Information information = dao.search(customer_mailaddress, customer_password);
+			//入力された情報が空でないことを確認
+			if (customer_mailaddress != null && !customer_mailaddress.isEmpty() &&
+					customer_password != null && !customer_password.isEmpty()) {
 
-			if (information != null) {
-				session.setAttribute("information", information);
-				request.getRequestDispatcher("/views/login-out_Customer.jsp").forward(request, response);
+				InformationDAO dao = new InformationDAO();
+
+				// まず既存の情報を検索
+				Information information = dao.search(customer_mailaddress, customer_password);
+
+				if (information == null) {
+					// 情報が存在しない場合、新規登録を行う
+					int result = dao.insert(customer_mailaddress, customer_password);
+
+					if (result > 0) {
+						// 既存情報がなければ登録ページに遷移
+						session.setAttribute("information", new Information());
+						request.getRequestDispatcher("/views/information_out.jsp").forward(request, response);
+					} else {
+						// 既存情報がすでにあればすでに登録されていると表示するページに遷移
+						request.getRequestDispatcher("/views/information_error.jsp").forward(request, response);
+					}
+				} else {
+					// 情報が既に存在する場合はエラーページに遷移
+					request.getRequestDispatcher("/views/information-error.jsp").forward(request, response);
+				}
 			} else {
-				request.getRequestDispatcher("/views/login-error.jsp").forward(request, response);
+				// 必要な情報が入力されていない場合、エラーページに遷移
+				request.getRequestDispatcher("/views/information-error.jsp").forward(request, response);
 			}
 		} catch (Exception e) {
 			e.printStackTrace(out);
 		}
 	}
-
 }
--- src/main/java/jp/co/aforce/dao/InformationDAO.java
+++ src/main/java/jp/co/aforce/dao/InformationDAO.java
@@ -2,29 +2,57 @@ package jp.co.aforce.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import jp.co.aforce.beans.Information;
 
 public class InformationDAO extends DAO {
-	public boolean insert(String customer_mailaddress, String customer_password)
+	public int insert(String customer_mailaddress, String customer_password)
 			throws Exception {
 
 		Connection con = getConnection();
 
 		PreparedStatement st;
-		st = con.prepareStatement("INSERT INTO Customer values (null, ?, ?)");
+		st = con.prepareStatement("INSERT INTO Customer(Customer_Mailaddress, Customer_Password) values (?, ?)");
 		st.setString(1, customer_mailaddress);
 		st.setString(2, customer_password);
-//		st.setString(3, customer_lastname);
-//		st.setString(4, customer_firstname);
-//		st.setString(5, customer_telephone);
-//		st.setString(6, customer_address);
-		int line = st.executeUpdate();
+		//		st.setString(3, customer_lastname);
+		//		st.setString(4, customer_firstname);
+		//		st.setString(5, customer_telephone);
+		//		st.setString(6, customer_address);
+		int result = st.executeUpdate();
 		
 		st.close();
 		con.close();
+
+		return result;
+
+	}
+
+	public Information search(String customer_mailaddress, String customer_password) throws Exception {
+		Connection con = getConnection();
+
+		PreparedStatement st = con
+				.prepareStatement("SELECT * FROM Customer WHERE Customer_Mailaddress = ? AND Customer_Password = ?");
+		st.setString(1, customer_mailaddress);
+		st.setString(2, customer_password);
+
+		ResultSet rs = st.executeQuery();
 		
-		return line > 0;
+		Information information = null;
+
+		if (rs.next()) {
+			information = new Information();
+			information.setCustomer_id(rs.getInt("customer_id"));
+			information.setCustomer_mailaddress(rs.getString("customer_mailaddress"));
+			information.setCustomer_password(rs.getString("customer_password"));
+		}
+
+		rs.close();
+		st.close();
+		con.close();
+
+		return information;
 	}
 
 }
