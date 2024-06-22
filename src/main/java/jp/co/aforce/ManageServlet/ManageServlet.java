23e1034a2c2a393ace3e080259cfa1715f14057a
Ayaka-k5 <a.kido@ks-japan.jp>
2024-06-22 18:33:54 +0900

    新規作成したものコミット（新規登録ページを確認したら、新たに追加したファイルをコミットしないとブランチを移動できなかったため）

---
+++ src/main/java/jp/co/aforce/ManageServlet/ManageServlet.java
@@ -0,0 +1,61 @@
+package jp.co.aforce.ManageServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jp.co.aforce.beans.Manage;
+import jp.co.aforce.dao.ManageDAO;
+
+@WebServlet("/manageServlet")
+public class ManageServlet extends HttpServlet {
+	@Override
+	protected void doPost(
+			HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		try {
+			
+			HttpSession session = request.getSession();
+			
+			String product_name = request.getParameter("product_name");
+			String product_priceParam = request.getParameter("product_price");
+			int product_price = 0;
+			String product_description = request.getParameter("product_description");
+			String product_stockParam = request.getParameter("product_stock");
+			int product_stock = 0;
+			try {
+				product_price = Integer.parseInt(product_priceParam);
+			} catch (NumberFormatException e) {
+				return;
+			}
+
+			try {
+				product_stock = Integer.parseInt(product_stockParam);
+			} catch (NumberFormatException e) {
+				return;
+			}
+			ManageDAO dao = new ManageDAO();
+
+			// 新規登録を行う
+			int result = dao.insert(product_name, product_price,
+					product_description, product_stock);
+			if (result > 0) {
+				// 登録に成功した場合、成功ページに遷移
+				session.setAttribute("manage", new Manage());
+				request.getRequestDispatcher("/views/manage-out.jsp").forward(request, response);
+			} else {
+				// 登録に失敗した場合、エラーページに遷移
+				request.getRequestDispatcher("/views/manage-error.jsp").forward(request, response);
+			}
+		} catch (Exception e) {
+			e.printStackTrace(out);
+			// 例外が発生した場合もエラーページに遷移
+			request.getRequestDispatcher("/views/manage-error.jsp").forward(request, response);
+		}
+	}
+}
---
+++ src/main/java/jp/co/aforce/logoutServlet/LogoutServlet_Manager.java
@@ -0,0 +1,33 @@
+package jp.co.aforce.logoutServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/logoutServlet_Manager")
+public class LogoutServlet_Manager extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+
+		try {
+			HttpSession session = request.getSession();
+
+			if (session.getAttribute("login_Manager") != null) {
+				session.removeAttribute("login_Manager");
+				request.getRequestDispatcher("/views/logout-out_Manager.jsp").forward(request, response);
+			} else {
+				request.getRequestDispatcher("/views/logout-error_Manager.jsp").forward(request, response);
+			}
+		} catch (Exception e) {
+			e.printStackTrace(out);
+		}
+	}
+}
