91ac2c25102db8b1ba1f23871f3746c91ca5f8ec
Ayaka-k5 <a.kido@ks-japan.jp>
2024-06-23 16:03:51 +0900

    バリデーション

--- src/main/java/jp/co/aforce/dao/InformationDAO_Customer.java
+++ src/main/java/jp/co/aforce/dao/InformationDAO_Customer.java
@@ -2,6 +2,7 @@ package jp.co.aforce.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class InformationDAO_Customer extends DAO {
 	public int insert(String customer_mailaddress, String customer_password, String customer_lastname,
@@ -10,6 +11,8 @@ public class InformationDAO_Customer extends DAO {
 
 		Connection con = getConnection();
 
+		con.setAutoCommit(false);
+		
 		PreparedStatement st;
 		st = con.prepareStatement(
 				"INSERT INTO Customer (Customer_Mailaddress, Customer_Password, Customer_LastName, Customer_FirstName, Customer_Telephone, Customer_Address) values (?, ?, ?, ?, ?, ?)");
@@ -20,6 +23,24 @@ public class InformationDAO_Customer extends DAO {
 		st.setString(5, customer_telephone);
 		st.setString(6, customer_address);
 		int result = st.executeUpdate();
+		
+		st = con.prepareStatement("SELECT * FROM Customer where Customer_Mailaddress = ?");
+		st.setString(1, customer_mailaddress);
+		ResultSet rs = st.executeQuery();
+		int line = 0;
+		while (rs.next()) {
+			line++;
+		}
+		
+		if (line == 1) {
+			con.commit();
+			System.out.println("登録しました。");
+		} else {
+			con.rollback();
+			System.out.println("すでに登録されています。");
+		}
+
+		con.setAutoCommit(true);
 
 		st.close();
 		con.close();
@@ -27,47 +48,4 @@ public class InformationDAO_Customer extends DAO {
 		return result;
 
 	}
-
-//	public Information_Customer search(String customer_mailaddress/*, String customer_password, String customer_lastname,
-//			String customer_firstname, String customer_telephone, String customer_address*/)
-//			throws Exception {
-//		Connection con = getConnection();
-//
-//		PreparedStatement st = con.prepareStatement(
-//				"SELECT * FROM Customer WHERE Customer_Mailaddress = ?");
-//		st.setString(1, customer_mailaddress);
-		/*st.setString(2, customer_password);
-		st.setString(3, customer_lastname);
-		st.setString(4, customer_firstname);
-		st.setString(5, customer_telephone);
-		st.setString(6, customer_address);*/
-
-//		ResultSet rs = st.executeQuery();
-//
-//		Information_Customer information_customer = null;
-//
-//		if (rs.next()) {
-//			information_customer = new Information_Customer();
-//			//information_customer.setCustomer_id(rs.getInt("customer_id"));
-//
-//			information_customer.setCustomer_mailaddress(rs.getString("customer_mailaddress"));
-
-			/*information_customer.setCustomer_password(rs.getString("customer_password"));
-
-			information_customer.setCustomer_lastname(rs.getString("customer_lastname"));
-
-			information_customer.setCustomer_firstname(rs.getString("customer_firstname"));
-
-			information_customer.setCustomer_telephone(rs.getString("customer_telephone"));
-
-			information_customer.setCustomer_address(rs.getString("customer_address"));
-//		}*/
-
-//		rs.close();
-//		st.close();
-//		con.close();
-//
-//		return information_customer;
-//	}
-
 }
